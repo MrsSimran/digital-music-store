4e51829b390e7664555bdacc6c096db6f3645f0f
Simran shaikh <simranshaikh30158@gmail.com>
2023-10-29 01:05:55 +0530

    adding color in box

--- color-boxjs.js
+++ color-boxjs.js
@@ -5,9 +5,27 @@ rect.addEventListener("mousemove", function (details) {
     var insiderectval = details.clients - rectangleloction.left;
 
     if (insiderectval < rectangleloction.width / 2) {
-        console.log("left");
+
+        var redcolor = gsap.utils.mapRange(0, rectangleloction.width / 2, 255, 0, insiderectval);
+
+        gsap.to(rect, {
+            backgroundColor: 'rbg(${redcolor},0,0)', ease: power4,
+        });
     }
     else {
-        console.log("right");
-    }
+
+        var bluecolor = gsap.utils.mapRange(rectangleloction.width / 2, rectangleloction.width, 0, 255, insiderectval);
+
+        gsap.to(rect, {
+            backgroundColor: 'rbg(0,0,${bluecolor})', ease: power4,
+        });
+
+
+        rect.addEventListener("mouseleave", function () {
+            gsap.to(rect, 
+           {backgroundColor: "white",
+            });
+        };
+       
 });
+
